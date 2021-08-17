8489b3b74ed55193259c467d8a2660c46fd34ed0
Joseph Son <sms0330@gmail.com>
2021-08-16 18:03:06 -0700

    Done to webpack_lab

--- src/index.js
+++
@@ -1,4 +0,0 @@
-// src/index.js
-import "./a.js";
-import "./b.js";
-console.log("Hello, World from Webpack");
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,61 @@
-// webpack.config.js
-module.exports = {
-    mode: "development"
-  };
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  // This is an array of entry points
+  //   entry: ["./src/index.js", "./src/users.js"],
+
+  //   This is single entry point
+  entry: {
+    // Create a bundle "./dist/client.js" (named after the key)
+    client: './src/client.js',
+    // Create a bundle "./dist/defaults.js" (named after the key)
+    defaults: './src/defaults.js',
+    server: './src/defaults.js',
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Demo",
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+  },
+};
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  if (isProduction) config.module.rules[0].options.name = "[contenthash].[ext]";
+
+  console.log("is production", isProduction);
+
+  config.devtool = isProduction ? "eval" : "source-map";
+  return config;
+};
